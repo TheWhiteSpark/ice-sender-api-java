3d7e2732d286da1c99cb758d98711673dc8d327d
TheWhiteSpark <thewhitespark6@gmail.com>
2018-04-10 15:18:34 +0200

    Database connection done

--- src/main/java/com/thewhitespark/iceSender/model/Test.java
+++ src/main/java/com/thewhitespark/iceSender/model/Test.java
@@ -1,10 +1,20 @@
 package com.thewhitespark.iceSender.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement
+@Entity
+@Table(name = "tests")
 public class Test {
+
+    @Id
+    @XmlTransient
+    public int id_tests;
 
     @XmlElement
     public String value;
--- src/main/java/com/thewhitespark/iceSender/rest/TestEndpoint.java
+++ src/main/java/com/thewhitespark/iceSender/rest/TestEndpoint.java
@@ -2,28 +2,30 @@ package com.thewhitespark.iceSender.rest;
 
 import com.thewhitespark.iceSender.model.Test;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.util.ArrayList;
 import java.util.List;
 
+@Stateless
 @Path("/tests")
 public class TestEndpoint {
+
+    @PersistenceContext(unitName = "myPU")
+    private EntityManager em;
 
     @GET
     @Produces({"application/json"})
     public List<Test> getAll() {
+        TypedQuery<Test> query = em.createQuery(
+                "SELECT t from Test t", Test.class
+        );
 
-        List<Test> testList = new ArrayList<Test>();
-        testList.add(new Test());
-        testList.add(new Test());
-        testList.add(new Test());
-
-        for (Test item : testList) {
-            item.value = "testVal";
-        }
-
+        List<Test> testList = query.getResultList();
         return testList;
     }
 }
